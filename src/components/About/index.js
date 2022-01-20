56596e62d7e29778d4c9e03d707978f939c414bc
inmarluna <inmarluna@Inmars-MacBook-Pro.local>
2022-01-19 18:14:56 -0600

    started setup for footer and about components

---
+++ src/components/About/index.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+function About() {
+  // jsx
+  return (
+    <section>
+      <h2> About Me </h2>
+      <img> /* profile photo */ </img>
+      <div>
+        <p>
+          I'm baby celiac DIY skateboard af hammock mlkshk snackwave austin
+          microdosing single-origin coffee neutra retro. Mumblecore austin
+          schlitz deep v pitchfork marfa. 3 wolf moon gochujang offal semiotics
+          tilde literally photo booth celiac street art master cleanse poutine.
+          90's hell of fanny pack seitan, etsy iceland kale chips unicorn
+          sriracha narwhal vinyl single-origin coffee jianbing. Try-hard
+          adaptogen XOXO, drinking vinegar roof party narwhal la croix cornhole
+          activated charcoal viral thundercats man bun echo park cliche. Cliche
+          trust fund kitsch, vice crucifix kombucha roof party farm-to-table
+          XOXO organic bicycle rights cornhole. Trust fund tofu DIY, godard
+          poutine pug salvia meh hoodie synth taiyaki mixtape. Edison bulb synth
+          live-edge glossier health goth. Chicharrones pickled intelligentsia
+          godard, 90's everyday carry narwhal authentic subway tile tofu
+          messenger bag kogi cornhole. Craft beer af tote bag, migas mumblecore
+          lumbersexual hoodie chartreuse shoreditch tbh master cleanse
+          dreamcatcher gochujang polaroid palo santo. Tacos chambray next level
+          roof party tofu sustainable twee echo park vexillologist put a bird on
+          it venmo. Freegan gluten-free marfa, stumptown tote bag crucifix
+          microdosing copper mug tumblr. XOXO tilde affogato man bun, banjo
+          aesthetic pug. Cliche man bun offal, mumblecore tacos blog hot chicken
+          migas selfies edison bulb pok pok. Activated charcoal health goth
+          helvetica, next level artisan pop-up squid readymade chillwave hot
+          chicken occupy. Direct trade franzen woke, readymade vaporware PBR&B
+          man braid
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default About;
---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function Footer() {
+    // jsx 
+    return (
+        <footer>
+            <div>
+                {/* // social links displayed with logos  */}
+            </div>
+            <h3> &copy; Copyright 2022 Inmar Luna </h3>
+        </footer>
+    )
+}
